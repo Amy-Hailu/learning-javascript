91b25464990306e9dfd3da90709688da80823a53
Amy-Hailu <amy.hailu@ndeverstech.com>
2023-11-16 22:29:36 -0500

    adds who is buying lunch project

--- Section 14/whoIsBuyingLunch.js
+++ Section 14/whoIsBuyingLunch.js
@@ -5,24 +5,18 @@
 function whosPaying(famList) {
 
     //get random index value 
-    var randomFamIndex=  Math.floor(Math.random() * famList.length);
-    
+    var randomFamIndex = Math.floor(Math.random() * famList.length);
+
     //get random item
     var randomFamName = famList[randomFamIndex];
-    
+
     return randomFamName;
-        
-    }
-    
-    var famList = ["Jay","Ruth","Raeye","Robel","Nathan","Eb"];
-    
-    var result = whosPaying(famList);
-    
-    console.log(result + " is going to buy lunch today!");
-
-
-
-
-
 
 }
+
+var famList = ["Jay", "Ruth", "Raeye", "Robel", "Nathan", "Eb"];
+
+var result = whosPaying(famList);
+
+console.log(result + " is going to buy lunch today!");
+
