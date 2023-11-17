4ead67d203997ce9b7b776c3a814a2fb3fcba098
Amy-Hailu <amy.hailu@ndeverstech.com>
2023-11-16 22:30:47 -0500

    updates fizzbuzz file

--- Section 14/fizzbuzz.js
+++ Section 14/fizzbuzz.js
@@ -1,8 +1,14 @@
 var output = [];
 var count = 1;
  
+function test(){
 
+
+    console.log("here");
+}
 function fizzBuzz(){
+
+    while(count <=100) {
 
      if(count % 3 === 0 && count % 5 === 0){
         output.push("fizzbuzz");
@@ -23,12 +29,14 @@ function fizzBuzz(){
         
     //output.push(count);
     count++;
+    }
     console.log(output);
 
 
 }
 
- 
+fizzBuzz();
+ test();
  
 
 
